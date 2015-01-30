a9d0a6dc548c1bca4a155197c8a83bcec714c93f
jicheol <jicheol@good4ryu.net>
2015-01-30 17:22:21 +0100

    remove unused import

--- src/hestia/huck/hestia/controller/accountbook/SlipController.java
+++ src/hestia/huck/hestia/controller/accountbook/SlipController.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.IntSummaryStatistics;
